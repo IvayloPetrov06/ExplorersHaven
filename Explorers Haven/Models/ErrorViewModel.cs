d74913f58e3d07239fdd89979864f57fbb291e4b
IvayloPetrov06 <ivayloveelinov@gmail.com>
2025-01-22 10:50:10 +0200

    Initial Setup

---
+++ Explorers Haven/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Explorers_Haven.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

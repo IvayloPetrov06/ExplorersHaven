6920b90f880e86853a80ba5ce32c9beac7169264
IvayloPetrov06 <ivayloveelinov@gmail.com>
2025-04-09 10:05:21 +0300

    View password fix

--- ExplorersHaven.DataAccess/Migrations/20250409070347_setup.Designer.cs
+++ ExplorersHaven.DataAccess/Migrations/20250409070347_setup.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Explorers_Haven.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250407030131_setup")]
+    [Migration("20250409070347_setup")]
     partial class setup
     {
         /// <inheritdoc />
